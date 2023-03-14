a5d2acbab920347669e72d0c79a8f5c1e59306b7
thiseaskambas <tkambas@yahoo.com>
2023-03-14 16:35:28 -0400

    starting Core class

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,1 +1,13 @@
+<?php
+/* 
+App Core Class :
+* Creates URL & loads main controller
+* URL FORMAT : /controller/method/params
+*/
 
+class Core
+{
+   protected $currentController = 'Pages';
+   protected $currentMethod = 'index';
+   protected $params = [];
+}
