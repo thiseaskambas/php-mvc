67fea9b66030c4af5d64f69886a7b04290eab6ba
thiseaskambas <tkambas@yahoo.com>
2023-03-14 18:21:59 -0400

    controllers are loaded based on url

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -13,11 +13,28 @@ class Core
 
    public function __construct()
    {
-      $this->getUrl();
+      // print_r($this->getUrl());
+      $url = $this->getUrl();
+      //Look into controllers for first value
+      if (!empty($url) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+         $this->currentController = ucwords($url[0]);
+         unset($url[0]);
+      }
+      //require controller
+      require_once '../app/controllers/' . $this->currentController . '.php';
+
+      //instantiate controller
+      $this->currentController = new $this->currentController();
    }
 
    public function getUrl()
    {
-      echo $_GET['url'];
+      if (isset($_GET['url'])) {
+         $url = rtrim($_GET['url'], '/');
+         //The FILTER_SANITIZE_URL filter removes all illegal URL characters from a string.
+         $url = filter_var($url, FILTER_SANITIZE_URL);
+         $url = explode('/', $url);
+         return $url;
+      }
    }
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once '../app/bootsrap.php';
 
 //Init Core Library
