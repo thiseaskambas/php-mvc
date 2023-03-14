f330c84971488c58ac13d4f0f2bf77e3ae816597
thiseaskambas <tkambas@yahoo.com>
2023-03-14 17:40:49 -0400

    getting the url

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,4 +10,14 @@ class Core
    protected $currentController = 'Pages';
    protected $currentMethod = 'index';
    protected $params = [];
+
+   public function __construct()
+   {
+      $this->getUrl();
+   }
+
+   public function getUrl()
+   {
+      echo $_GET['url'];
+   }
 }
--- public/index.php
+++ public/index.php
@@ -1,2 +1,6 @@
 <?php
 require_once '../app/bootsrap.php';
+
+//Init Core Library
+
+$init = new Core();
