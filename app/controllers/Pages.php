4ef47f569cdff79c2a15f37d2b9da6719081217e
thiseaskambas <tkambas@yahoo.com>
2023-03-15 19:10:58 -0400

    core framework, about to cleanup

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,12 +1,20 @@
 <?php
 class Pages extends Controller
 {
+   public $postModel;
+
    public function __construct()
    {
+      $this->postModel = $this->model('Post');
    }
    public function index()
    {
-      $data = ['title' => 'Welcome'];
+      $posts = $this->postModel->getPosts();
+      $data = [
+         'title' => 'Welcome',
+         'posts' => $posts
+      ];
+
       $this->view("pages/index", $data);
    }
    public function about()
